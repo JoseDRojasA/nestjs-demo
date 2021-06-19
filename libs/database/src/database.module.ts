e4bc613dd9cb9636b09a3a648986f1d776baef5a
Jose David Rojas Aguilar <jose42829@gmail.com>
2021-06-19 15:30:13 -0500

    Clean project

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule, Configuration } from '@app/common';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DatabaseService } from './database.service';
 
 const { host, port, username, password, database } = Configuration.api.db;
 
@@ -18,7 +17,5 @@ const { host, port, username, password, database } = Configuration.api.db;
       synchronize: false,
     }),
   ],
-  providers: [DatabaseService],
-  exports: [DatabaseService],
 })
 export class DatabaseModule {}
