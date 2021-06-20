092901aefde97c4fa594c342d31dd63dd25d1fbc
Jose David Rojas Aguilar <jose42829@gmail.com>
2021-06-20 02:35:22 -0500

    Add lint

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,9 +1,7 @@
 import { CommonModule, Configuration } from '@app/common';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { values } from 'lodash';
 import { Product, ProductMovement } from './entities';
-import * as repositories from './repositories';
 
 const { host, port, username, password, database, logging } =
   Configuration.api.db;
